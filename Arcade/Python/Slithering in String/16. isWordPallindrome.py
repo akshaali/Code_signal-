3c2c5ea5690037cd9e95f542a60e6d0f675f51d7
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2019-02-28 00:30:11 +0530

    Create 16. isWordPallindrome.py

---
+++ Arcade/Python/Slithering in String/16. isWordPallindrome.py
@@ -0,0 +1,31 @@
+""""
+Given a word, check whether it is a palindrome or not. A string is considered to be a palindrome if it reads the same in both directions.
+
+Example
+
+For word = "aibohphobia", the output should be
+isWordPalindrome(word) = true;
+
+For word = "hehehehehe", the output should be
+isWordPalindrome(word) = false.
+
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] string word
+
+A string containing lowercase English letters.
+
+Guaranteed constraints:
+1 ≤ word.length ≤ 20.
+
+[output] boolean
+
+true if the given word is a palindrome, false otherwise.
+
+""""
+
+
+def isWordPalindrome(word):
+    return word == word[::-1]
