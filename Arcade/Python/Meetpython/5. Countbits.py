a370fe2459fe075e84bced160f9b55985427f324
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2019-02-28 00:16:51 +0530

    Create 5. Countbits.py

---
+++ Arcade/Python/Meetpython/5. Countbits.py
@@ -0,0 +1,21 @@
+""""
+Implement the missing code, denoted by ellipses. You may not modify the pre-existing code.
+Implement a function that, given an integer n, uses a specific method on it and returns the number of bits in its binary representation.
+Note: in this task and most of the following tasks you will be given a code snippet with some part of it replaced by the ellipsis (...). Only this part is allowed to be changed.
+Example
+For n = 50, the output should be
+countBits(n) = 6.
+5010 = 1100102, a number that consists of 6 digits. Thus, the output should be 6.
+Input/Output
+[execution time limit] 4 seconds (py3)
+[input] integer n
+A positive integer.
+Guaranteed constraints:
+1 ≤ n ≤ 109.
+[output] integer
+The number of bits in binary representation of n.
+""""
+
+
+def countBits(n):
+    return n.bit_length()
