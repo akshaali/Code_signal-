532884409a35047649b84cb836053169bc02ae56
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2019-02-28 00:27:03 +0530

    Create 14. ConvertTabs.py

---
+++ Arcade/Python/Slithering in String/14. ConvertTabs.py
@@ -0,0 +1,15 @@
+""""
+You found an awesome customizable Python IDE that has almost everything you'd like to see in your working environment. However, after a couple days of coding you discover that there is one important feature that this IDE lacks: it cannot convert tabs to spaces. Luckily, the IDE is easily customizable, so you decide to write a plugin that would convert all tabs in the code into the given number of whitespace characters.
+
+Implement a function that, given a piece of code and a positive integer x will turn each tabulation character in code into x whitespace characters.
+
+Example
+
+For code = "\treturn False" and x = 4, the output should be
+convertTabs(code, x) = "    return False".
+
+""""
+
+def convertTabs(code, x):
+    return (' '*x).join(code.split('\t'))
+
