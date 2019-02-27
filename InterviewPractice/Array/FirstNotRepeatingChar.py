651db4cadea817a85ac61ff3eb2de68d4cd9ef28
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2019-02-27 23:00:58 +0530

    Create FirstNotRepeatingChar.py

---
+++ InterviewPractice/Array/FirstNotRepeatingChar.py
@@ -0,0 +1,31 @@
+""""
+Note: Write a solution that only iterates over the string once and uses O(1) additional memory, since this is what you would be asked to do during a real interview.
+
+Given a string s consisting of small English letters, find and return the first instance of a non-repeating character in it. If there is no such character, return '_'.
+
+Example
+
+For s = "abacabad", the output should be
+firstNotRepeatingCharacter(s) = 'c'.
+
+There are 2 non-repeating characters in the string: 'c' and 'd'. Return c since it appears in the string first.
+
+For s = "abacabaabacaba", the output should be
+firstNotRepeatingCharacter(s) = '_'.
+
+There are no characters in this string that do not repeat.
+""""
+
+
+def firstNotRepeatingCharacter(s):
+    print(set(list(s)))
+    l = []
+    for i in set(s):
+        print(s.count(i))
+        if s.count(i) == 1:
+            l.append(s.index(i))
+    if l == []:
+        return "_"
+    else:
+        return s[min(l)]
+
