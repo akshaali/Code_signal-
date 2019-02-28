58a73ce80818f4fe687904d18f50b694f0fbd230
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2019-02-28 14:03:58 +0530

    Create ChessTeams.py

---
+++ Arcade/Python/Fumbling in function/ChessTeams.py
@@ -0,0 +1,20 @@
+""""
+
+A grand Team Chess Tournament will be held at your University. Two teams, smarties and cleveries, will clash to determine whose chess skills are better. The teams have the same number of members, and the ith member of smarties will play against the ith member of cleveries in the ith game for each valid i
+
+Given the names of the players of both smarties and cleveries, return the games in the order they will be played.
+
+Example
+
+For smarties = ["Jane", "Bob", "Peter"] and
+cleveries = ["Oscar", "Lidia", "Ann"], the output should be
+
+chessTeams(smarties, cleveries) = [["Jane",  "Oscar"],
+                                   ["Bob",   "Lidia"],
+                                   ["Peter", "Ann"]]
+""""
+
+
+
+def chessTeams(smarties, cleveries):
+    return list(zip(smarties,cleveries))
