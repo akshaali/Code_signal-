3bb5c8b1416eed0c9afdfbb44d112a4c70a96fb7
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2019-03-02 22:14:35 +0530

    Create 3. checkPallindrome.cpp

---
+++ Arcade/Intro/The journey Begins/3. checkPallindrome.cpp
@@ -0,0 +1,29 @@
+""""
+
+Given the string, check if it is a palindrome.
+
+Example
+
+For inputString = "aabaa", the output should be
+checkPalindrome(inputString) = true;
+For inputString = "abac", the output should be
+checkPalindrome(inputString) = false;
+For inputString = "a", the output should be
+checkPalindrome(inputString) = true.
+
+""""
+std:: string
+bool chec (intstr){
+char intstr[22] ;
+int length=strlen(intstr);
+int i;
+for(i=0 ; i< length ; i++)
+{
+if(intstr[1]=intstr(length-1-i))
+return(false)
+else
+return(true)
+}
+
+
+}
