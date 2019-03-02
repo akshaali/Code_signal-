40bb9233eaf9c6389393416cbe14aa8472cbce2d
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2019-03-03 01:24:05 +0530

    Create reverseNodesInKGroup.py

---
+++ InterviewPractice/Linked Lists/reverseNodesInKGroup.py
@@ -0,0 +1,37 @@
+""""
+Given a linked list l, reverse its nodes k at a time and return the modified list. k is a positive integer that is less than or equal to the length of l. If the number of nodes in the linked list is not a multiple of k, then the nodes that are left out at the end should remain as-is.
+
+You may not alter the values in the nodes - only the nodes themselves can be changed.
+
+Example
+
+For l = [1, 2, 3, 4, 5] and k = 2, the output should be
+reverseNodesInKGroups(l, k) = [2, 1, 4, 3, 5];
+For l = [1, 2, 3, 4, 5] and k = 1, the output should be
+reverseNodesInKGroups(l, k) = [1, 2, 3, 4, 5];
+For l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11] and k = 3, the output should be
+reverseNodesInKGroups(l, k) = [3, 2, 1, 6, 5, 4, 9, 8, 7, 10, 11].
+
+""""
+
+# Definition for singly-linked list:
+# class ListNode(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.next = None
+#
+def reverseNodesInKGroups(l, k):
+    lis = []
+    while l is not None:
+        lis.append(l.value)
+        l = l.next
+    output = []
+    for i in range(0,len(lis),k):
+        j = i+k
+        temp = lis[i:j]
+        if len(temp) < k:
+            output += temp[:]
+        else:
+            output +=temp[::-1]
+    return output
+
