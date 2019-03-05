bf10469340fb410e6cd3b001d37e99d79b72ea3f
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2019-03-05 08:36:51 +0530

    Create circleOfNumbers.py

---
+++ Arcade/Intro/Through the Fog/circleOfNumbers.py
@@ -0,0 +1,21 @@
+""""
+Consider integer numbers from 0 to n - 1 written down along the circle in such a way that the distance between any two neighboring numbers is equal (note that 0 and n - 1 are neighboring, too).
+
+Given n and firstNumber, find the number which is written in the radially opposite position to firstNumber.
+
+Example
+
+For n = 10 and firstNumber = 2, the output should be
+circleOfNumbers(n, firstNumber) = 7.
+
+""""
+
+def circleOfNumbers(n, firstNumber):
+    arr = list(range(n))
+    if firstNumber < n//2:
+        return arr[firstNumber+(n//2)]
+    if firstNumber > n//2 :
+        return arr[firstNumber-(n//2)]
+    if firstNumber ==  n//2:
+        return arr[0]
+
