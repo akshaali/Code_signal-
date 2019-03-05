887b1b8ab0779783d6b8b8c1e26f95f096aababc
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2019-03-05 18:53:23 +0530

    Create firstDigit.py

---
+++ Arcade/Intro/Diving Deep/firstDigit.py
@@ -0,0 +1,17 @@
+"""
+Find the leftmost digit that occurs in a given string.
+
+Example
+
+For inputString = "var_1__Int", the output should be
+firstDigit(inputString) = '1';
+For inputString = "q2q-q", the output should be
+firstDigit(inputString) = '2';
+For inputString = "0ss", the output should be
+firstDigit(inputString) = '0'.
+""""
+def firstDigit(inputString):
+    for i in inputString:
+        if i.isdigit():
+            return i
+
