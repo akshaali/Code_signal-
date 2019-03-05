1f366aa7c296282b6611bc3322dbeb0cd35b6c98
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2019-03-05 08:27:18 +0530

    Create avoidObstacles.py

---
+++ Arcade/Intro/Island of knowledge/avoidObstacles.py
@@ -0,0 +1,23 @@
+""""
+You are given an array of integers representing coordinates of obstacles situated on a straight line.
+
+Assume that you are jumping from the point with coordinate 0 to the right. You are allowed only to make jumps of the same length represented by some integer.
+
+Find the minimal length of the jump enough to avoid all the obstacles.
+
+Example
+
+For inputArray = [5, 3, 6, 7, 9], the output should be
+avoidObstacles(inputArray) = 4.
+
+""""
+
+def avoidObstacles(inputArray):
+
+    for i in range(1, max(inputArray)):
+        divs = any([x for x in inputArray if not x%i])
+        if not divs:
+            return i
+    
+    return max(inputArray) + 1
+
