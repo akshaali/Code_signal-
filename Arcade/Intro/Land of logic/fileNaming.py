6d5675d4edf17e8a42cc12e12a10d2615e7d398b
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2019-03-12 07:59:47 +0530

    Create fileNaming.py

---
+++ Arcade/Intro/Land of logic/fileNaming.py
@@ -0,0 +1,25 @@
+"""
+You are given an array of desired filenames in the order of their creation. Since two files cannot have equal names, the one which comes later will have an addition to its name in a form of (k), where k is the smallest positive integer such that the obtained name is not used yet.
+
+Return an array of names that will be given to the files.
+
+Example
+
+For names = ["doc", "doc", "image", "doc(1)", "doc"], the output should be
+fileNaming(names) = ["doc", "doc(1)", "image", "doc(1)(1)", "doc(2)"].
+"""
+def fileNaming(names):
+    res = []
+    for name in names:
+        if name in res:
+            x = 1
+            tmp = name[:] + "(" + str(x) + ")"
+            while tmp in res:
+                x += 1
+                tmp = name[:] + "(" + str(x) + ")"
+            
+            res.append(tmp)
+        else:
+            res.append(name)
+            
+    return res
