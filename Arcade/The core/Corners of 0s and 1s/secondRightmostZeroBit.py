634cf330b54d797f09dfa2acf90f0df370daaeaa
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2019-03-19 22:55:44 +0530

    Create secondRightmostZeroBit.py

---
+++ Arcade/The core/Corners of 0s and 1s/secondRightmostZeroBit.py
@@ -0,0 +1,15 @@
+"""
+Presented with the integer n, find the 0-based position of the second rightmost zero bit in its binary representation (it is guaranteed that such a bit exists), counting from right to left.
+
+Return the value of 2position_of_the_found_bit.
+
+Example
+
+For n = 37, the output should be
+secondRightmostZeroBit(n) = 8.
+
+3710 = 1001012. The second rightmost zero bit is at position 3 (0-based) from the right in the binary representation of n.
+Thus, the answer is 23 = 8.
+"""
+def secondRightmostZeroBit(n):
+    return 2**bin(n)[::-1].find('0', bin(n)[::-1].find('0') + 1)
