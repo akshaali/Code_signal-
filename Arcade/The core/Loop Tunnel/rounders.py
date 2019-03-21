d45ae4209396ff1f37e8db5a43ff8c8fbf77b7f9
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2019-03-21 13:42:25 +0530

    Create rounders.py

---
+++ Arcade/The core/Loop Tunnel/rounders.py
@@ -0,0 +1,34 @@
+"""
+We want to turn the given integer into a number that has only one non-zero digit using a tail rounding approach. This means that at each step we take the last non 0 digit of the number and round it to 0 or to 10. If it's less than 5 we round it to 0 if it's larger than or equal to 5 we round it to 10 (rounding to 10 means increasing the next significant digit by 1). The process stops immediately once there is only one non-zero digit left.
+
+Example
+
+For n = 15, the output should be
+rounders(n) = 20;
+
+For n = 1234, the output should be
+rounders(n) = 1000.
+
+1234 -> 1230 -> 1200 -> 1000.
+
+For n = 1445, the output should be
+rounders(n) = 2000.
+
+1445 -> 1450 -> 1500 -> 2000.
+"""
+
+def rounders(n):
+    lst = list(int(i) for i in str(n))
+    lst = lst[::-1]
+    for i in range(len(lst)-1):
+        if lst[i] < 5:
+            lst[i] = 0
+        if lst[i] >= 5:
+            lst[i] = 0
+            lst[i+1] = lst[i+1] + 1
+    st = ''
+    for x in lst[::-1]:
+        st +=str(x)
+    return int(st)
+    
+
