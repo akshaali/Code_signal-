99458c71c79b63a3727dca9ee878d55ad0b45f3b
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2019-04-13 00:13:20 +0530

    Create simplifyPath.py

---
+++ InterviewPractice/Stack, Heap, Queues/simplifyPath.py
@@ -0,0 +1,34 @@
+"""
+Given an absolute file path (Unix-style), shorten it to the format /<dir1>/<dir2>/<dir3>/....
+
+Here is some info on Unix file system paths:
+
+/ is the root directory; the path should always start with it even if it isn't there in the given path;
+/ is also used as a directory separator; for example, /code/fights denotes a fights subfolder in the code folder in the root directory;
+this also means that // stands for "change the current directory to the current directory"
+. is used to mark the current directory;
+.. is used to mark the parent directory; if the current directory is root already, .. does nothing.
+Example
+
+For path = "/home/a/./x/../b//c/", the output should be
+simplifyPath(path) = "/home/a/b/c".
+
+Here is how this path was simplified:
+* /./ means "move to the current directory" and can be replaced with a single /;
+* /x/../ means "move into directory x and then return back to the parent directory", so it can replaced with a single /;
+* // means "move to the current directory" and can be replaced with a single /.
+"""
+
+def simplifyPath(path):
+    p = path.split('/')
+    s = []
+    for x in p:
+        if not x:
+            continue
+        if x == '..':
+            if s:
+                s.pop()
+        elif x != '.':
+            s.append(x)
+    return '/'+'/'.join(s)
+
